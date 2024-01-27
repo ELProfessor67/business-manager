40fc69810bd36556d613c4ef4af51ee14952ebd9
elprofessor67 <jeeshanr599@gmail.com>
2024-01-27 00:16:26 -0800

    change some code

--- src/app/api/scan/route.js
+++ src/app/api/scan/route.js
@@ -1,13 +1,17 @@
 import { NextResponse } from 'next/server';
 import { execSync } from 'child_process';
 
+
 export async function POST(req) {
   const { url } = await req.json();
 
   try {
-    const command = `npx lighthouse ${url} --output=json --only-categories=performance --chrome-flags="--headless"`;
+    console.log(process.env.CHROME_PATh)
+    // const command = `npx lighthouse ${url} --output=json --only-categories=performance --chrome-flags="--headless"`;
+    const command = `npx lighthouse ${url} --output=json --only-categories=performance --chrome-flags="--headless --no-sandbox" --chrome-path="${process.env.CHROMIUM_PATH}"`;
     let result = execSync(command, { encoding: 'utf-8' });
     result = JSON.parse(result);
+
     console.log(result)
 
     return NextResponse.json(result,{status: 200})
@@ -16,3 +20,5 @@ export async function POST(req) {
     return NextResponse.json(error,{status: 501})
   }
 }
+// npx lighthouse ${url} --output=json --only-categories=performance --chrome-flags="--headless --no-sandbox" --chrome-path="/snap/bin/chromium" --output-path=report.json
+// npx lighthouse https://www.geeksforgeeks.org/how-to-install-chrome-in-ubuntu/ --output=json --only-categories=performance --chrome-flags="--headless"
